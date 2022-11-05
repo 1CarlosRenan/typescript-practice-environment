01cb3d18c2f259d7f122e89eedaa2de2bfc1d6aa
1CarlosRenan <carlosrenan96@gmail.com>
2022-11-05 01:15:41 -0300

    arquivos base e tsconfig

---
+++ dist/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// string, boolean, number, ...
+let x = 10;
+console.log(x);
+// inferencia x annotation
+let y = 20;
+console.log(y);
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// string, boolean, number, ...
+let x: number = 10;
+
+console.log(x);
+
+// inferencia x annotation
+
+let y = 20;
+
+console.log(y)
